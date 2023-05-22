6736f5e1141edf5c9d6f466df0c704f8caab35d1
Jokicpuga <125294344+Jokicpuga@users.noreply.github.com>
2023-05-22 16:15:32 -0500

    Create Aplicacion3.java

---
+++ Aplicacion3.java
@@ -0,0 +1,35 @@
+package ec.edu.intsuperior.vista;
+
+public class Aplicacion {
+	
+	public class Animal
+	{
+	    private String raza;
+	    private String nombre;
+	    private int edad;
+
+}
+ public class Moto{
+	 private String color;
+	 private String cilindraje;
+	 private String velocidadaMaxima;
+ }
+  public class Mochila{
+	  private String numeroBonsillos;
+	  private String tamaño;
+	  private String color;
+  }
+  public class  Zapato{
+	  private String color;
+	  private String marca;
+	  private String precio;
+	  private String forma;
+  }
+  public class Empleado{
+	  private codempleado;
+	  private nomempleado;
+	  private apelempleado;
+	  private dirempleado;
+  }
+	  
+  }
